f6eb82f45239be3fcf0c2da8a66294a3b4149571
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-12-30 08:39:57 +0000

    [add]削除機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,14 +9,21 @@ class BooksController < ApplicationController
   end
 
   def index
-    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
 
-   private
+  private
 
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:create, :index, :show]
+  resources :books, only: [:create, :index, :show,:destroy]
 
 end
