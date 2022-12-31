c829c12f7cc9dc06e9e2818570126a86d13c68ab
tkhsmasa <115441873+tkhsmasa@users.noreply.github.com>
2022-12-31 08:30:53 +0000

    [add]機能の完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,9 +21,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    Book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:create, :index, :show,:destroy,:edit,:updade]
-
+  resources :books, only: [:create, :index, :show, :destroy, :edit, :updade]
+  resources :users, only: [:show, :edit, :update]
 end
